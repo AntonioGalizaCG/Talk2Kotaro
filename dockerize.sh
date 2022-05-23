5c241244799da2ba6b5ef035f14438404a5a37c1
AntonioGalizaCG <antonio.galiza@protonmail.com>
2022-05-23 16:49:24 +0900

    Removed useless files

--- dockerize.sh
+++
@@ -1,3 +0,0 @@
-docker build -t talk-with-kotaro:latest .
-docker save talk-with-kotaro:latest > ./talk-with-kotaro.tar
-scp -r ./talk-with-kotaro.tar antonio@mizuuchi2.lab.tuat.ac.jp:talk-with-kotaro.tar
